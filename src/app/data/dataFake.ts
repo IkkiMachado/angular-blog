cde3f8f35ff9b9d0a4dce2d946da05388189b53f
Ikki <hique.reis@hotmail.com>
2024-02-15 03:13:34 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover": "https://images.justwatch.com/backdrop/175706323/s640/moulin-rouge-amor-em-vermelho.%7Bformat%7D",
+    "title":  "SATINE MORRE DE TUBERCULOSE",
+    "description": "Grande estrela do famoso Moulin Rouge morre em apresentação após diagnostico de tuberculose"
+  },
+  {
+    "id":"2",
+    "photoCover": "https://www.rbsdirect.com.br/imagesrc/24892335.jpg?w=1200&h=630&a=c&version=1575255600",
+    "title":  "BURLESQUE NA BROADWAY!",
+    "description": "ola"
+  }
+]
